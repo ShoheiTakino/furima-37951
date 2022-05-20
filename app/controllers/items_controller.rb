a3f1924859fa2e2a1fc189ea565d0656335da133
ShoheiTakino <shohei0522penguin@gmail.com>
2022-05-20 21:14:44 +0900

    出品後にデータベースに商品情報保存成功

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,10 +9,16 @@ class ItemsController < ApplicationController
   
   def create 
     @item = Item.new(item_params)
+    if @item.save
+     redirect_to root_path
+    else
+      render  :new
+    end
   end
   
   private
   def item_params
+    params.require(:item).permit(:image, :name, :content, :category_id, :status_id, :status_id, :shipfee_id, :province_id, :shipping_id, :price).merge(user_id: current_user.id)
   end
 
 
