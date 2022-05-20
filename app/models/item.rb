9e526b64cfcd801c2f41fdeaed002f6fc3848d8d
ShoheiTakino <shohei0522penguin@gmail.com>
2022-05-20 17:17:09 +0900

    モデルへのバリテーション記入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
-  
+
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name,        presence: true
+  validates :content,     presence: true
+  validates :category_id, presence: true
+  validates :status_id,   presence: true
+  validates :shipfee_id,  presence: true
+  validates :province_id, presence: true
+  validates :date_i,      presence: true
+  validates :price,       presence: true
+  validates :image,       presence: true
+
 end
