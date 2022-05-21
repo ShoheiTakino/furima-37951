07bcf36d387e7a09f42811633fc9a0c43dec6202
ShoheiTakino <shohei0522penguin@gmail.com>
2022-05-21 14:21:44 +0900

    販売手数料の反映

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price"); 
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = inputValue/10
+      const tax = addTaxDom.innerHTML
+      const taxPrice = Math.floor(tax);
+      console.log(taxPrice);
+    });
 });
