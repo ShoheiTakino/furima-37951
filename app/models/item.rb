4761a7ea27fb8501fa2edd19b06af094d06af206
ShoheiTakino <shohei0522penguin@gmail.com>
2022-05-23 00:58:08 +0900

    商品発送負担表示の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,12 @@
 class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :status, :shipfee, :province, :date
+  belongs_to :category
+  belongs_to :status 
+  belongs_to :province 
+  belongs_to :date
+  belongs_to :shipping
+  belongs_to :shipfee
 
   belongs_to :user
   has_one_attached :image
@@ -10,7 +15,7 @@ class Item < ApplicationRecord
   validates :content,     presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
   validates :status_id,   presence: true, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :shipfee_id,  presence: true, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :shipfee_id,  numericality: { other_than: 1, message: "can't be blank" } 
   validates :province_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
   validates :shipping_id, presence: true, numericality: { other_than: 1, message: "can't be blank" } 
   validates :price,       presence: true, numericality: { only_integer: true, greater_than_or_equal_to:300, less_than_or_equal_to:9999999}
