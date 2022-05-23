cf70bba636471140dd4e75c89ba707dcf4e2d20f
ShoheiTakino <shohei0522penguin@gmail.com>
2022-05-23 16:48:30 +0900

    itemコントローラーでの削除許可条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,10 @@ class ItemsController < ApplicationController
 
   def destroy
      item = Item.find(params[:id])
+     if current_user.id == item.user.id
      item.destroy
      redirect_to root_path
-     
+     end
   end
 
   
