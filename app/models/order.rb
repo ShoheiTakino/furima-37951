69fa15acfebbd16fc71aa390660d42caa93efabd
ShoheiTakino <shohei0522penguin@gmail.com>
2022-05-25 00:18:13 +0900

    FormオブジェクトとPAY.JP実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,13 @@
 class Order < ApplicationRecord
 
+  attr_accessor :token
+
   belongs_to :item
   belongs_to :user
   has_one :address
 
-  validates: user, presence: true
-  validates: item, presence: true
+  validates :user,  presence: true
+  validates :item,  presence: true
+  validates :token, presence: true
   
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :province_id, :city, :address1, :address2, :phone, :order_id, :user_id, :item_id
+  attr_accessor :postal_code, :province_id, :city, :address1, :address2, :phone, :user_id, :order_id, :item_id
 
 
   
@@ -9,14 +9,13 @@ class OrderAddress
   validates :city,        presence: true
   validates :address1,    presence: true
   validates :phone,       presence: true
-  validates :order_id,    presence: true
   validates :user_id,     presence: true
   validates :item_id,     presence: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
 
-    Address.create(postal_code: postal_code, province_id: province_id, city: city, address1: address1, address2: address2, phone: phone, order_id: order_id )
+    Address.create(postal_code: postal_code, province_id: province_id, city: city, address1: address1, address2: address2, phone: phone, order_id: order.id)
   end
 
 end
