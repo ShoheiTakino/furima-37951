38fddf555653f2fb873bac1250098e50c7e3e275
ShoheiTakino <shohei0522penguin@gmail.com>
2022-05-24 11:20:37 +0900

    order_address周りのコントローラーとビューの記述追記

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,10 +5,24 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def new
+    @order_address = OrderAddress.new
+  end
+
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private 
 
+  def order_params
+    params.require(:order_address).permit(:postal_code, :province_id, :city, :address1, :address2, :phone).merge(user_id: current_user.id)
+  end
 
 end
