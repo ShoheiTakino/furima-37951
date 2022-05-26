561135b6c3cc77f13bdd03b0799b6898982f15c7
ShoheiTakino <shohei0522penguin@gmail.com>
2022-05-26 14:50:26 +0900

    orderコントローラーと商品詳細ページのリファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,9 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
   before_action :move_to_index
   before_action :authenticate_user!
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
@@ -14,7 +14,6 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
@@ -36,8 +35,11 @@ class OrdersController < ApplicationController
     )
   end
 
-  def move_to_index
+  def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
     redirect_to root_path if user_signed_in? && @item.user_id == current_user.id
 
     redirect_to root_path if @item.order.present?
